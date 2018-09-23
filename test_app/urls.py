67d307974e782e7011d6a5e69b8af2f304165c6d
Martin Kjellberg <martin@martinkjellberg.com>
2018-09-23 20:14:27 +0200

    Use Django 1.8+ style for static files

--- test_app/urls.py
+++ test_app/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
@@ -10,11 +11,4 @@ urlpatterns = [
     url(r'^webhooks/', include('trello_webhooks.urls')),
 ]
 
-if settings.DEBUG is False:
-    urlpatterns += [
-        url(
-            r'^static/(?P<path>.*)$',
-            static.serve,
-            {'document_root': settings.STATIC_ROOT}
-        ),
-    ]
+urlpatterns += staticfiles_urlpatterns()
